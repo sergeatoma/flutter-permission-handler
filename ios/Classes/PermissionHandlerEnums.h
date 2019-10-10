e122b6945fd092956524e62415d4edb2e1fe1672
Sergey Pankov <serge.p@atoma.at>
2019-10-10 20:30:56 +0300

    1. Removed unused permissions

--- ios/Classes/PermissionHandlerEnums.h
+++ ios/Classes/PermissionHandlerEnums.h
@@ -6,22 +6,10 @@
 //
 
 typedef NS_ENUM(int, PermissionGroup) {
-    PermissionGroupCalendar = 0,
-    PermissionGroupCamera,
-    PermissionGroupContacts,
-    PermissionGroupLocation,
-    PermissionGroupLocationAlways,
-    PermissionGroupLocationWhenInUse,
+
+    PermissionGroupCamera = 0,
     PermissionGroupMediaLibrary,
-    PermissionGroupMicrophone,
-    PermissionGroupPhone,
     PermissionGroupPhotos,
-    PermissionGroupReminders,
-    PermissionGroupSensors,
-    PermissionGroupSms,
-    PermissionGroupSpeech,
-    PermissionGroupStorage,
-    PermissionGroupIgnoreBatteryOptimizations,
     PermissionGroupUnknown,
 };
 
